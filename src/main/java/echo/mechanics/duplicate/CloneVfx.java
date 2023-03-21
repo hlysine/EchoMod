ce7855255e03716269d64a9707ab6b7675de34d4
Henry Lin <henry.ys.lin@gmail.com>
2023-03-21 11:17:28 +0800

    Fix SingleCardViewPopup

--- src/main/java/echo/mechanics/duplicate/CloneVfx.java
+++ src/main/java/echo/mechanics/duplicate/CloneVfx.java
@@ -1,7 +1,7 @@
 package echo.mechanics.duplicate;
 
 import basemod.BaseMod;
-import basemod.interfaces.PreDungeonUpdateSubscriber;
+import basemod.interfaces.PreUpdateSubscriber;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
@@ -20,7 +20,7 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class CloneVfx implements PreDungeonUpdateSubscriber {
+public class CloneVfx implements PreUpdateSubscriber {
     private static final Logger logger = LogManager.getLogger(CloneVfx.class);
     private static final ShaderProgram gridShader;
     private static final ShaderProgram cardShader;
@@ -61,6 +61,8 @@ public class CloneVfx implements PreDungeonUpdateSubscriber {
             int blendDstFunc = sb.getBlendDstFunc();
             Color color = sb.getColor();
             ShaderProgram shader = sb.getShader();
+            sb.flush();
+
             sb.end();
             fbo.begin();
             Gdx.gl.glClearColor(0, 0, 0, 0);
@@ -79,6 +81,8 @@ public class CloneVfx implements PreDungeonUpdateSubscriber {
 
     public static void playerPostRender(AbstractPlayer player, SpriteBatch sb) {
         if (CloningModule.isCloning()) {
+            sb.flush();
+
             sb.end();
             fbo.end();
             sb.begin();
@@ -103,6 +107,8 @@ public class CloneVfx implements PreDungeonUpdateSubscriber {
 
     public static void relicPreRender(AbstractRelic relic, SpriteBatch sb) {
         if (CloningModule.relicTransformer != null && !CloningModule.relicTransformer.originalRelics.contains(relic)) {
+            sb.flush();
+
             gridShader.begin();
             gridShader.setUniformf("res_x", 128);
             gridShader.setUniformf("res_y", 128);
@@ -124,6 +130,8 @@ public class CloneVfx implements PreDungeonUpdateSubscriber {
 
     public static void cardPreRender(AbstractCard card, SpriteBatch sb) {
         if (card.color == Echo.Enums.CARD_COLOR) {
+            sb.flush();
+
             cardShader.begin();
             cardShader.setUniformf("res_x", 512);
             cardShader.setUniformf("res_y", 512);
@@ -143,7 +151,7 @@ public class CloneVfx implements PreDungeonUpdateSubscriber {
     }
 
     @Override
-    public void receivePreDungeonUpdate() {
+    public void receivePreUpdate() {
         cloneVfxTimer += Gdx.graphics.getDeltaTime();
     }
 }
--- src/main/java/echo/patches/cards/CardShaderPatch.java
+++ src/main/java/echo/patches/cards/CardShaderPatch.java
@@ -28,7 +28,7 @@ public class CardShaderPatch {
     )
     public static class PortraitGridCardBackground {
         public static void Prefix(SingleCardViewPopup __instance, SpriteBatch sb, AbstractCard ___card) {
-            CloneVfx.cardPreRender(___card, sb); // todo: buggy white region, background dim is gone
+            CloneVfx.cardPreRender(___card, sb);
         }
 
         public static void Postfix(SingleCardViewPopup __instance, SpriteBatch sb, AbstractCard ___card) {
