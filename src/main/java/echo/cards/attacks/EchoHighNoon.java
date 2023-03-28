ab8c7f85cd4c93ea2127369eb12013167e06598e
Henry Lin <henry.ys.lin@gmail.com>
2023-03-28 16:35:39 +0800

    Add CassidyCopy, EchoHighNoon

---
+++ src/main/java/echo/cards/attacks/EchoHighNoon.java
@@ -0,0 +1,33 @@
+package echo.cards.attacks;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import echo.EchoMod;
+import echo.cards.AbstractBaseCard;
+
+public class EchoHighNoon extends AbstractBaseCard {
+
+    public static final String ID = EchoMod.makeID(EchoHighNoon.class.getSimpleName());
+
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+
+    public EchoHighNoon() {
+        super(ID, TARGET);
+
+        this.isMultiDamage = true;
+        this.selfRetain = true;
+        this.exhaust = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAllEnemiesAction(p, this.multiDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.NONE, true));
+    }
+
+    @Override
+    public void onRetained() {
+        upgradeDamage(this.magicNumber);
+    }
+}
