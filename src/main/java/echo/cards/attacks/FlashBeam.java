813a5334eec8c1a1016ba111d95587b48102a312
Henry Lin <henry.ys.lin@gmail.com>
2023-04-02 17:41:04 +0800

    Add FlashBeam

---
+++ src/main/java/echo/cards/attacks/FlashBeam.java
@@ -0,0 +1,28 @@
+package echo.cards.attacks;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import echo.EchoMod;
+import echo.cards.FocusedCard;
+import echo.mechanics.focused.FocusedChecker;
+
+public class FlashBeam extends FocusedCard {
+
+    public static final String ID = EchoMod.makeID(FlashBeam.class.getSimpleName());
+
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+
+    public FlashBeam() {
+        super(ID, TARGET, true);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (FocusedChecker.isFocused(m)) {
+            addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SMASH));
+        }
+    }
+}
