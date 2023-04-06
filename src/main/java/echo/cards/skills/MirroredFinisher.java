271cd5b6b8a90e41737c411a57c05fa2d1412444
Henry Lin <henry.ys.lin@gmail.com>
2023-04-06 21:31:25 +0800

    Add MirroredFinisher

---
+++ src/main/java/echo/cards/skills/MirroredFinisher.java
@@ -0,0 +1,35 @@
+package echo.cards.skills;
+
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import echo.EchoMod;
+import echo.actions.duplicate.DuplicateRandomPlayerAction;
+import echo.cards.FocusedCard;
+import echo.characters.Echo;
+import echo.mechanics.focused.FocusedChecker;
+import echo.subscribers.AfterCardUseSubscriber;
+
+public class MirroredFinisher extends FocusedCard implements AfterCardUseSubscriber {
+
+    public static final String ID = EchoMod.makeID(MirroredFinisher.class.getSimpleName());
+
+    private static final CardTarget TARGET = CardTarget.SELF;
+
+    public MirroredFinisher() {
+        super(ID, TARGET, true);
+
+        this.exhaust = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+    }
+
+    @Override
+    public void afterUse() {
+        if (FocusedChecker.anyFocused()) {
+            addToBot(new DuplicateRandomPlayerAction(new AbstractPlayer.PlayerClass[]{Echo.Enums.ECHO, AbstractDungeon.player.chosenClass}, false));
+        }
+    }
+}
