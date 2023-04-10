d6ac826332eed52e53e30a423006c6f7f4d68276
Henry Lin <henry.ys.lin@gmail.com>
2023-04-10 10:08:54 +0800

    Fix stacking logic of FastGenerationPower

--- src/main/java/echo/powers/FastGenerationPower.java
+++ src/main/java/echo/powers/FastGenerationPower.java
@@ -62,7 +62,7 @@ public class FastGenerationPower extends AbstractPower implements NonStackablePo
         if (target != this.owner && damageAmount > 0 && CloningModule.isCloning()) {
             flashWithoutSound();
             damageDealt += damageAmount;
-            int powerAmount = Math.floorDiv(damageAmount, damagePerCharge);
+            int powerAmount = Math.floorDiv(damageAmount, damagePerCharge) * amount;
             damageDealt = Math.floorMod(damageDealt, damagePerCharge);
             if (powerAmount > 0) {
                 addToBot(new ApplyPowerAction(owner, owner, new UltimateChargePower(owner, powerAmount)));
