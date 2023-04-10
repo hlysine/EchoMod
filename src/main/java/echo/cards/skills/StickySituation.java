4da85106cd8397c2da5ad025131c5cd3064dd58f
Henry Lin <henry.ys.lin@gmail.com>
2023-04-10 10:16:24 +0800

    Add StickySituation

---
+++ src/main/java/echo/cards/skills/StickySituation.java
@@ -0,0 +1,38 @@
+package echo.cards.skills;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.vfx.ThoughtBubble;
+import echo.EchoMod;
+import echo.cards.AbstractBaseCard;
+import echo.patches.cards.CustomCardTags;
+import echo.powers.StickyBombPower;
+import echo.util.RunnableAction;
+
+public class StickySituation extends AbstractBaseCard {
+
+    public static final String ID = EchoMod.makeID(StickySituation.class.getSimpleName());
+
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+
+    public StickySituation() {
+        super(ID, TARGET);
+
+        this.tags.add(CustomCardTags.STICK);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new RunnableAction(() -> {
+            int count = (int) m.powers.stream().filter(pow -> pow.type == AbstractPower.PowerType.DEBUFF).count();
+            if (count > 0) {
+                addToBot(new ApplyPowerAction(m, p, new StickyBombPower(m, count * magicNumber)));
+            } else {
+                AbstractDungeon.effectList.add(new ThoughtBubble(AbstractDungeon.player.dialogX, AbstractDungeon.player.dialogY, 3.0F, cardStrings.EXTENDED_DESCRIPTION[0], true));
+            }
+        }));
+    }
+}
