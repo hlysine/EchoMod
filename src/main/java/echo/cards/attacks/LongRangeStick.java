4e5ff4c73792890d1c71d651d4acb00b020b11bc
Henry Lin <henry.ys.lin@gmail.com>
2023-04-20 14:03:45 +0800

    Rework LongRangeStick to be an Attack

--- src/main/java/echo/cards/attacks/LongRangeStick.java
+++ src/main/java/echo/cards/attacks/LongRangeStick.java
@@ -1,6 +1,9 @@
-package echo.cards.skills;
+package echo.cards.attacks;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -26,6 +29,7 @@ public class LongRangeStick extends AbstractBaseCard {
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
         addToBot(new RunnableAction(() -> {
             if (p.powers.stream().anyMatch(pow -> pow instanceof FlightPower)) {
                 addToBot(new ApplyPowerAction(m, p, new StickyBombPower(m, magicNumber)));
