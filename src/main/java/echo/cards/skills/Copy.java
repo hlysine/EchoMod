4f67a37df434843bbd0abe404c531a5303330ae0
Henry Lin <henry.ys.lin@gmail.com>
2023-04-23 10:46:13 +0800

    Buff Copy: cost reduced from 2(1) to 1, add 4(6) ult charge gain when uncharged

--- src/main/java/echo/cards/skills/Copy.java
+++ src/main/java/echo/cards/skills/Copy.java
@@ -1,11 +1,15 @@
 package echo.cards.skills;
 
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import echo.EchoMod;
 import echo.actions.duplicate.DuplicateRandomPlayerAction;
 import echo.cards.ChargedCard;
 import echo.characters.Echo;
+import echo.mechanics.duplicate.ChargedChecker;
+import echo.powers.UltimateChargePower;
 import echo.subscribers.AfterCardUseSubscriber;
 
 public class Copy extends ChargedCard implements AfterCardUseSubscriber {
@@ -14,12 +18,26 @@ public class Copy extends ChargedCard implements AfterCardUseSubscriber {
 
     private static final CardTarget TARGET = CardTarget.SELF;
 
+    private boolean charged;
+
     public Copy() {
-        super(ID, TARGET, true);
+        super(ID, TARGET, false);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        charged = ChargedChecker.isCharged();
+        if (charged)
+            super.use(p, m);
     }
 
     @Override
     public void afterUse() {
-        addToBot(new DuplicateRandomPlayerAction(new AbstractPlayer.PlayerClass[]{Echo.Enums.ECHO, AbstractDungeon.player.chosenClass}));
+        AbstractPlayer p = AbstractDungeon.player;
+        if (charged) {
+            addToBot(new DuplicateRandomPlayerAction(new AbstractPlayer.PlayerClass[]{Echo.Enums.ECHO, p.chosenClass}));
+        } else {
+            addToBot(new ApplyPowerAction(p, p, new UltimateChargePower(p, magicNumber)));
+        }
     }
 }
