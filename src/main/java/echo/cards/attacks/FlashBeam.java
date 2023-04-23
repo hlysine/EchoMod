5e06a954c5bbf4d1839e5d08ceca7e128a6c6f72
Henry Lin <henry.ys.lin@gmail.com>
2023-04-23 16:40:45 +0800

    Buff FlashBeam: add 1 card draw when not focused

--- src/main/java/echo/cards/attacks/FlashBeam.java
+++ src/main/java/echo/cards/attacks/FlashBeam.java
@@ -2,11 +2,13 @@ package echo.cards.attacks;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import echo.EchoMod;
 import echo.cards.FocusedCard;
+import echo.mechanics.focused.FocusedChecker;
 
 public class FlashBeam extends FocusedCard {
 
@@ -15,11 +17,15 @@ public class FlashBeam extends FocusedCard {
     private static final CardTarget TARGET = CardTarget.ENEMY;
 
     public FlashBeam() {
-        super(ID, TARGET, true);
+        super(ID, TARGET, false);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SMASH));
+        if (FocusedChecker.isFocused(m)) {
+            addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SMASH));
+        } else {
+            addToBot(new DrawCardAction(magicNumber));
+        }
     }
 }
