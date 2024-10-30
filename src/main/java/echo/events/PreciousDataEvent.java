50b8f58ac8009430635297f7a8ae67571430012a
Henry Lin <henry.ys.lin@gmail.com>
2024-10-30 20:36:53 +0800

    Rework the Precious Data event: loss max HP for each Deepfake obtained

--- src/main/java/echo/events/PreciousDataEvent.java
+++ src/main/java/echo/events/PreciousDataEvent.java
@@ -3,7 +3,7 @@ package echo.events;
 import basemod.BaseMod;
 import basemod.abstracts.events.PhasedEvent;
 import basemod.abstracts.events.phases.TextPhase;
-import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
@@ -16,6 +16,7 @@ import echo.cards.skills.Deepfake;
 import echo.mechanics.duplicate.Duplicator;
 import echo.mechanics.duplicate.PlayerClassManager;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -27,62 +28,79 @@ public class PreciousDataEvent extends PhasedEvent {
     private static final String[] DESCRIPTIONS = eventStrings.DESCRIPTIONS;
     private static final String[] OPTIONS = eventStrings.OPTIONS;
     private static final String TITLE = eventStrings.NAME;
-    private static final int CARD_COUNT = 3;
-    private static final int DAMAGE = 15;
+    private static final int DAMAGE = 2;
+    private static final int DAMAGE_SCALE = 2;
 
     private static final String ENCOUNTER_PHASE = "Encounter";
-    private static final String DECISION_PHASE = "Decision";
     private static final String ACCEPTED_PHASE = "Accepted";
+    private static final String[] DECISION_PHASE = {"Decision 1", "Decision 2", "Decision 3", ACCEPTED_PHASE};
     private static final String REJECTED_PHASE = "Rejected";
 
-    private final List<AbstractPlayer> playerChoices;
-    private final Deepfake previewCard = new Deepfake();
-    private AbstractPlayer chosenPlayer;
+    List<Deepfake> obtainedCards = new ArrayList<>();
+    int hpLoss = 0;
 
     public PreciousDataEvent() {
         super(ID, TITLE, IMG);
 
-        playerChoices = PlayerClassManager.getClassList(AbstractDungeon.eventRng, new AbstractPlayer.PlayerClass[]{Duplicator.getTrueClass()}, 3, false)
-                .stream().map(BaseMod::findCharacter).collect(Collectors.toList());
-
-        TextPhase encounterPhase = new TextPhase(DESCRIPTIONS[0]);
-        for (AbstractPlayer choice : playerChoices) {
-            encounterPhase.addOption(
-                    OPTIONS[0] + Arrays.stream(choice.title.split(" ")).map(s -> "#y" + s).collect(Collectors.joining(" ")) + OPTIONS[1],
-                    i -> {
-                        chosenPlayer = playerChoices.get(i);
-                        previewCard.targetClass = chosenPlayer.chosenClass;
-                        previewCard.initializeDescription();
-                        transitionKey(DECISION_PHASE);
-                    }
-            );
-        }
-        registerPhase(ENCOUNTER_PHASE, encounterPhase);
-
-        registerPhase(DECISION_PHASE, new TextPhase(DESCRIPTIONS[1])
-                .addOption(new TextPhase.OptionInfo(OPTIONS[2] + CARD_COUNT + OPTIONS[3] + DAMAGE + OPTIONS[4], previewCard), i -> {
-                    Deepfake card = new Deepfake(chosenPlayer.chosenClass);
-                    AbstractDungeon.effectList.add(new ShowCardAndObtainEffect(card.makeStatEquivalentCopy(), Settings.WIDTH / 2f, Settings.HEIGHT / 2f));
-                    AbstractDungeon.effectList.add(new ShowCardAndObtainEffect(card.makeStatEquivalentCopy(), Settings.WIDTH / 2f, Settings.HEIGHT / 2f));
-                    AbstractDungeon.effectList.add(new ShowCardAndObtainEffect(card.makeStatEquivalentCopy(), Settings.WIDTH / 2f, Settings.HEIGHT / 2f));
-                    CardCrawlGame.screenShake.shake(ScreenShake.ShakeIntensity.MED, ScreenShake.ShakeDur.MED, false);
-                    CardCrawlGame.sound.play("BLUNT_FAST");
-                    AbstractDungeon.player.damage(new DamageInfo(null, DAMAGE));
-                    logMetricObtainCardAndDamage(ID, chosenPlayer.id, card, DAMAGE);
-                    transitionKey(ACCEPTED_PHASE);
+        registerPhase(ENCOUNTER_PHASE, new TextPhase(DESCRIPTIONS[0])
+                .addOption(OPTIONS[0], i -> {
+                    obtainedCards.clear();
+                    hpLoss = 0;
+                    transitionKey(DECISION_PHASE[0]);
                 })
+        );
+
+        for (int phase = 0; phase < 3; phase++) {
+            final int finalPhase = phase;
+            List<AbstractPlayer> playerChoices = PlayerClassManager.getClassList(AbstractDungeon.eventRng, new AbstractPlayer.PlayerClass[]{Duplicator.getTrueClass()}, 3, false)
+                    .stream().map(BaseMod::findCharacter).collect(Collectors.toList());
+
+            TextPhase encounterPhase = new TextPhase(DESCRIPTIONS[1 + finalPhase]);
+            for (AbstractPlayer choice : playerChoices) {
+                Deepfake previewCard = new Deepfake(choice.chosenClass);
+                previewCard.initializeDescription();
+                encounterPhase.addOption(
+                        new TextPhase.OptionInfo(OPTIONS[1] + Arrays.stream(choice.title.split(" ")).map(s -> "#y" + s).collect(Collectors.joining(" ")) + OPTIONS[2] + DAMAGE + OPTIONS[3], previewCard),
+                        i -> {
+                            AbstractDungeon.effectList.add(new ShowCardAndObtainEffect(previewCard.makeStatEquivalentCopy(), Settings.WIDTH / 2f, Settings.HEIGHT / 2f));
+                            CardCrawlGame.screenShake.shake(ScreenShake.ShakeIntensity.MED, ScreenShake.ShakeDur.MED, false);
+                            CardCrawlGame.sound.play("BLUNT_FAST");
+                            AbstractDungeon.player.decreaseMaxHealth(DAMAGE + DAMAGE_SCALE * finalPhase);
+
+                            obtainedCards.add(previewCard);
+                            hpLoss += DAMAGE + DAMAGE_SCALE * finalPhase;
+
+                            transitionKey(DECISION_PHASE[finalPhase + 1]);
+                        }
+                );
+            }
+            encounterPhase.addOption(OPTIONS[4], i -> {
+                if (finalPhase == 0) {
+                    transitionKey(REJECTED_PHASE);
+                } else {
+                    transitionKey(ACCEPTED_PHASE);
+                }
+            });
+            registerPhase(DECISION_PHASE[phase], encounterPhase);
+        }
+
+        registerPhase(ACCEPTED_PHASE, new TextPhase(DESCRIPTIONS[4])
+                .addOption(OPTIONS[5], i -> {
+                    logMetricObtainCardsLoseMapHP(
+                            ID,
+                            obtainedCards.stream().map(AbstractCard::getMetricID).collect(Collectors.joining(", ")),
+                            obtainedCards.stream().map(c -> c.cardID).collect(Collectors.toList()),
+                            hpLoss
+                    );
+                    openMap();
+                })
+        );
+
+        registerPhase(REJECTED_PHASE, new TextPhase(DESCRIPTIONS[5])
                 .addOption(OPTIONS[5], i -> {
                     logMetricIgnored(ID);
-                    transitionKey(REJECTED_PHASE);
+                    openMap();
                 })
-        );
-
-        registerPhase(ACCEPTED_PHASE, new TextPhase(DESCRIPTIONS[2])
-                .addOption(OPTIONS[6], i -> openMap())
-        );
-
-        registerPhase(REJECTED_PHASE, new TextPhase(DESCRIPTIONS[3])
-                .addOption(OPTIONS[6], i -> openMap())
         );
 
         transitionKey(ENCOUNTER_PHASE);
