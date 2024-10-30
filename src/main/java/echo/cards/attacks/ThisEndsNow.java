1ddf197a2dd5c76c28c51867953300ded324b6b7
Henry Lin <henry.ys.lin@gmail.com>
2024-10-30 12:04:39 +0800

    Buff This Ends Now: deal 130 damage all enemies

--- src/main/java/echo/cards/attacks/ThisEndsNow.java
+++ src/main/java/echo/cards/attacks/ThisEndsNow.java
@@ -2,8 +2,7 @@ package echo.cards.attacks;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import echo.EchoMod;
@@ -14,11 +13,12 @@ public class ThisEndsNow extends ChargedCard {
 
     public static final String ID = EchoMod.makeID(ThisEndsNow.class.getSimpleName());
 
-    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
 
     public ThisEndsNow() {
         super(ID, TARGET, true);
 
+        this.isMultiDamage = true;
         this.exhaust = true;
         this.isEthereal = true;
     }
@@ -27,6 +27,6 @@ public class ThisEndsNow extends ChargedCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         super.use(p, m);
         addToBot(new VFXAction(new EchoOverclockEffect()));
-        addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.NONE));
+        addToBot(new DamageAllEnemiesAction(p, this.multiDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.NONE, true));
     }
 }
