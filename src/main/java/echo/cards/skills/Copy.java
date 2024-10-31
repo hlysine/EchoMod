7d150be32f17fd68bc9716165ae07a04039d91d6
Henry Lin <henry.ys.lin@gmail.com>
2024-10-31 19:52:46 +0800

    Buff Copy: always give Ultimate Charge, then duplicate if charged

--- src/main/java/echo/cards/skills/Copy.java
+++ src/main/java/echo/cards/skills/Copy.java
@@ -1,6 +1,7 @@
 package echo.cards.skills;
 
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -18,26 +19,30 @@ public class Copy extends ChargedCard implements AfterCardUseSubscriber {
 
     private static final CardTarget TARGET = CardTarget.SELF;
 
-    private boolean charged;
-
     public Copy() {
         super(ID, TARGET, false);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        charged = ChargedChecker.isCharged();
-        if (charged)
-            super.use(p, m);
+        addToBot(new ApplyPowerAction(p, p, new UltimateChargePower(p, magicNumber)));
+    }
+
+    @Override
+    public void triggerOnGlowCheck() {
+        if (ChargedChecker.isCharged() || ChargedChecker.baseCharged(10 - magicNumber)) {
+            this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+        } else {
+            this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+        }
     }
 
     @Override
     public void afterUse() {
         AbstractPlayer p = AbstractDungeon.player;
-        if (charged) {
+        if (ChargedChecker.isCharged()) {
+            super.use(p, null);
             addToBot(new DuplicateRandomPlayerAction(new AbstractPlayer.PlayerClass[]{Duplicator.getTrueClass(), p.chosenClass}));
-        } else {
-            addToBot(new ApplyPowerAction(p, p, new UltimateChargePower(p, magicNumber)));
         }
     }
 }
